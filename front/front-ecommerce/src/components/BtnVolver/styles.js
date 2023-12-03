93fabe11ac45b6229681673d080a7edcc4e9b033
diegohp <diego.blake11@gmail.com>
2023-12-03 14:19:43 -0600

    [FIX] duplicated GlobalStyles files

--- front/front-ecommerce/src/components/BtnVolver/styles.js
+++ front/front-ecommerce/src/components/BtnVolver/styles.js
@@ -1,19 +1,19 @@
 import { styled } from 'styled-components';
-import { textFont } from '../../assets/styles/GlobalStyles';
+import { textFont } from '../../styles/GlobalStyles';
 
 const Button = styled.button`
-    display: flex;
-    background-color: #D9D9D9;
-    color: black;
-    padding-inline: 0px;
-    cursor:pointer;
-    p{
-        padding-inline: 15px;
-        font-family: ${textFont};
-    }
-    &:hover{
-        border-color: black;
-    }
+  display: flex;
+  background-color: #d9d9d9;
+  color: black;
+  padding-inline: 0px;
+  cursor: pointer;
+  p {
+    padding-inline: 15px;
+    font-family: ${textFont};
+  }
+  &:hover {
+    border-color: black;
+  }
 `;
 
-export { Button }
+export { Button };
--- front/front-ecommerce/src/components/Volver/styles.js
+++ front/front-ecommerce/src/components/Volver/styles.js
@@ -1,50 +1,50 @@
 import { styled } from 'styled-components';
-import { titleFont, textFont } from '../../assets/styles/GlobalStyles';
+import { titleFont, textFont } from '../../styles/GlobalStyles';
 
 const ContainerVolver = styled.div`
-    max-width: 100%;
-    margin: 40px;
-    background-color: #D9D9D9;
-    color: black;
-    padding-inline:40px;
-    @media (max-width: 576px) {
-        margin-inline: 0px;
-        padding-inline:20px;
-    }
+  max-width: 100%;
+  margin: 40px;
+  background-color: #d9d9d9;
+  color: black;
+  padding-inline: 40px;
+  @media (max-width: 576px) {
+    margin-inline: 0px;
+    padding-inline: 20px;
+  }
 `;
 
 const DivVolver = styled.div`
-    height: 80px;
-    display: flex;
-    font-size: 2rem;
-    font-family: ${titleFont};
-    align-items: center;
+  height: 80px;
+  display: flex;
+  font-size: 2rem;
+  font-family: ${titleFont};
+  align-items: center;
 `;
 
 const DivTitulo = styled.div`
-    height: 80px;
-    display: flex;
-    font-size: 2rem;
-    font-family: ${titleFont};
-    align-items: center;
-    @media (max-width: 576px) {
-        justify-content: center
-    }
+  height: 80px;
+  display: flex;
+  font-size: 2rem;
+  font-family: ${titleFont};
+  align-items: center;
+  @media (max-width: 576px) {
+    justify-content: center;
+  }
 `;
 
 const Button = styled.button`
-    display: flex;
-    background-color: #D9D9D9;
-    color: black;
-    padding-inline: 0px;
-    cursor:pointer;
-    p{
-        padding-inline: 15px;
-        font-family: ${textFont};
-    }
-    &:hover{
-        border-color: black;
-    }
+  display: flex;
+  background-color: #d9d9d9;
+  color: black;
+  padding-inline: 0px;
+  cursor: pointer;
+  p {
+    padding-inline: 15px;
+    font-family: ${textFont};
+  }
+  &:hover {
+    border-color: black;
+  }
 `;
 
-export { ContainerVolver, DivVolver, DivTitulo, Button}
+export { ContainerVolver, DivVolver, DivTitulo, Button };
