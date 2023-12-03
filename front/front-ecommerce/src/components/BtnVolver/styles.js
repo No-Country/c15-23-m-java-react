2bb7209afee15b3a06900ba4f28a042cb392ee09
kevin joel noviello <kjnoviello@gmail.com>
2023-12-03 20:27:47 -0300

    fix conflict with PR

--- front/front-ecommerce/src/components/BtnVolver/styles.js
+++ front/front-ecommerce/src/components/BtnVolver/styles.js
@@ -4,8 +4,8 @@ import { textFont } from '../../styles/GlobalStyles';
 const Button = styled.button`
   display: flex;
   background-color: #d9d9d9;
-  color: black;
-  padding-inline: 0px;
+  // color: black;
+  // padding-inline: 0px;
   cursor: pointer;
   p {
     padding-inline: 15px;
@@ -13,6 +13,21 @@ const Button = styled.button`
   }
   &:hover {
     border-color: black;
+  }
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 0em;
+  font-size: 1em;
+  // font-weight: 500;
+  font-family: inherit;
+  // background-color: #1a1a1a;
+  transition: border-color 0.25s;
+  button:hover {
+  border-color: #646cff;
+  }
+  button:focus,
+  button:focus-visible {
+  outline: 4px auto -webkit-focus-ring-color;
   }
 `;
 
