654dec057cf9f26c293bf47d225a1b28d887c3f4
Lucas Von Elm <70777709+xlucasve@users.noreply.github.com>
2023-12-04 11:22:07 -0300

    Revert "Iniciado producto"

--- back/product-service/src/main/java/c15_23_m_java_react/com/product_service/dtos/ProductDTO.java
+++
@@ -1,5 +0,0 @@
-package c15_23_m_java_react.com.product_service.dtos;
-
-public class ProductDTO {
-
-}
--- back/product-service/src/main/java/c15_23_m_java_react/com/product_service/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package c15_23_m_java_react.com.product_service.repository;
-
-import c15_23_m_java_react.com.product_service.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
