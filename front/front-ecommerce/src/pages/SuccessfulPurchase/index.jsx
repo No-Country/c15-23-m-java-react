3b2591d07d31b0c97a194200c1659a63d976d389
kevin joel noviello <kjnoviello@gmail.com>
2023-12-18 10:24:21 -0300

    add empty cart and limit add button

--- front/front-ecommerce/src/pages/SuccessfulPurchase/index.jsx
+++ front/front-ecommerce/src/pages/SuccessfulPurchase/index.jsx
@@ -1,14 +1,16 @@
 import { OrderSummary } from '../../components/OrderSummary';
-import { Card, Content, Main, Section, Summary } from './styles';
+import { Card, Content, Main, Section, Summary, SectionBtn } from './styles';
 import { useContext } from 'react';
 import { AppContext } from '../../context/AppContext';
 import { useEffect, useState } from 'react'
 import { getUser } from '../../api/getUser'; 
 import LoadingOrder from '../../components/LoadingOrder/LoadingOrder.jsx';
+import { NavLink } from 'react-router-dom';
 
 const SuccessfulPurchase = () => {
   const {
-    state: { cart }
+    state: { cart },
+    emptyCart,
   } = useContext(AppContext);
   const totalPrice = cart.reduce((acc, el) => 
      acc + el.quantity * el.price, 0
@@ -33,51 +35,56 @@ const SuccessfulPurchase = () => {
     <>
      {loadingOrder ? 
       <LoadingOrder /> 
-      : <Main>
+      : <>
+        <Main>
         <Section>
-          <Card>
-            <h1>Orden realizada con éxito</h1>
-            <h2>¡Gracias por comprar con nosotros!</h2>
-            <p>
-              Apreciamos tu compra. Para obtener tus productos debes dirigirte a
-              la tienda para realizar el pago con el método que has seleccionado y
-              ahí se te entregarán tus productos.
-            </p>
-            <Content>
-              <div>
+            <Card>
+              <h1>Orden realizada con éxito</h1>
+              <h2>¡Gracias por comprar con nosotros!</h2>
+              <p>
+                Apreciamos tu compra. Para obtener tus productos debes dirigirte a
+                la tienda para realizar el pago con el método que has seleccionado y
+                ahí se te entregarán tus productos.
+              </p>
+              <Content>
                 <div>
-                  <p>Resumen de la compra</p>
-                  <p>Orden número: 38380 </p>
-                  <p>Fecha: 20 Diciembre 2023 </p>
-                  <p>Total: $ {totalPrice}</p>
+                  <div>
+                    <p>Resumen de la compra</p>
+                    <p>Orden número: 38380 </p>
+                    <p>Fecha: 20 Diciembre 2023 </p>
+                    <p>Total: $ {totalPrice}</p>
+                  </div>
+                  <div>
+                    <p>Método de pago</p>
+                    <p>Tarjeta de crédito</p>
+                  </div>
                 </div>
-                <div>
-                  <p>Método de pago</p>
-                  <p>Tarjeta de crédito</p>
-                </div>
-              </div>
 
-              <div>
                 <div>
-                  <p>Dirección</p>
-                  <p>{user && (
-                      <p>{user?.direccion?.charAt(0).toUpperCase() + user?.direccion?.slice(1)}
-                </p>)}
-                </p>
+                  <div>
+                    <p>Dirección</p>
+                    <p>{user && (
+                        <p>{user?.direccion?.charAt(0).toUpperCase() + user?.direccion?.slice(1)}
+                  </p>)}
+                  </p>
+                  </div>
+                  <div>
+                    <p>¿Alguna pregunta? </p>
+                    <p>Contáctanos</p>
+                    <p>Visita nuestra página de FAQ</p>
+                  </div>
                 </div>
-                <div>
-                  <p>¿Alguna pregunta? </p>
-                  <p>Contáctanos</p>
-                  <p>Visita nuestra página de FAQ</p>
-                </div>
-              </div>
-            </Content>
-          </Card>
-        </Section>
-        <Summary>
-          <OrderSummary />
-        </Summary>
-      </Main>}
+              </Content>
+            </Card>
+          </Section>
+          <Summary>
+            <OrderSummary />
+          </Summary>
+        </Main>
+        <SectionBtn>
+          <NavLink to='/'><button onClick={()=>emptyCart()}>Ir al Home</button></NavLink>
+        </SectionBtn>
+      </>}
     </>
   )
 };
