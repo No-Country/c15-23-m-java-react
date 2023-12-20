e038abd6c305b61904bf7344c8672e34231a21e2
diegohp <diego.blake11@gmail.com>
2023-12-20 02:17:17 -0600

    [UPDATE] change the button text when the client adds a product

--- front/front-ecommerce/src/components/Modal/Modal.jsx
+++ front/front-ecommerce/src/components/Modal/Modal.jsx
@@ -1,30 +1,52 @@
 /* eslint-disable react/prop-types */
-import {ItemImgCard} from '../ItemCard/styles'
-import {ContainerBodyModal, ModalBody, AddButton} from './styles';
+import { ItemImgCard } from '../ItemCard/styles';
+import { ContainerBodyModal, ModalBody, AddButton } from './styles';
 import { useContext } from 'react';
 import { AppContext } from '../../context/AppContext';
 import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-function ModalItemCard({ handleClose, show, name, imageLink, description, price, brand, active, category, availableStock, id }) {
-
+function ModalItemCard({
+  handleClose,
+  show,
+  name,
+  imageLink,
+  description,
+  price,
+  brand,
+  active,
+  category,
+  availableStock,
+  id,
+}) {
   const {
-    addToCart
+    state: { cart },
+    addToCart,
   } = useContext(AppContext);
+
+  const [added, setAdded] = useState(false);
+
+  useEffect(() => {
+    const inCart = cart.find((item) => item.id === id);
+    if (inCart) {
+      setAdded(true);
+    }
+  }, [cart]);
 
   return (
     <>
       <Modal
         show={show}
         onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
+        backdrop='static'
+        keyboard={false}>
         <Modal.Header closeButton>
           <Modal.Title>Detalles</Modal.Title>
         </Modal.Header>
         <ModalBody>
           <ItemImgCard>
-              <img style={{width: '150px', height: '150px'}} src={imageLink}/>
+            <img style={{ width: '150px', height: '150px' }} src={imageLink} />
           </ItemImgCard>
           <ContainerBodyModal>
             <div>
@@ -32,31 +54,39 @@ function ModalItemCard({ handleClose, show, name, imageLink, description, price,
               {/* <p>{brand}</p> */}
             </div>
             <div>
-              <i><p>{description}</p></i>
+              <i>
+                <p>{description}</p>
+              </i>
             </div>
             <div>
-              <h2><strong>${price}</strong></h2>
-              <p>Disponibilidad {availableStock} u.</p>
+              <h2>
+                <strong>${price}</strong>
+              </h2>
+              <p>Disponibilidad: {availableStock} u.</p>
               <p>{category}</p>
             </div>
-            <AddButton onClick={() => {
-              addToCart({
-                id,
-                name,
-                imageLink,
-                description,
-                price,
-                brand,
-                active,
-                category,
-                availableStock,
-                quantity: 1,
-              });
-            }}>Agregar</AddButton>
+            <AddButton
+              className={added ? 'added' : ''}
+              onClick={() => {
+                addToCart({
+                  id,
+                  name,
+                  imageLink,
+                  description,
+                  price,
+                  brand,
+                  active,
+                  category,
+                  availableStock,
+                  quantity: 1,
+                });
+              }}>
+              {added ? 'Agregado' : 'Agregar'}
+            </AddButton>
           </ContainerBodyModal>
         </ModalBody>
         <Modal.Footer>
-          <AddButton variant="success" onClick={handleClose}>
+          <AddButton variant='success' onClick={handleClose}>
             Cerrar
           </AddButton>
         </Modal.Footer>
